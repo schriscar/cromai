0e415a07f0dde88e3d4c9dd3f9092cdb271bdcf9
chris <33089502+schriscar@users.noreply.github.com>
2019-12-14 22:17:32 -0300

    Update cromai_bash.sh

--- cromai_bash.sh
+++ cromai_bash.sh
@@ -11,46 +11,19 @@
 [ -e "cromai.log" ] && rm cromai.log
 
 laco_infinito()
-while true; do
-executa=true
-	
-# Lista de pids python ativos.   
-lista=$(pidof -x python 2>> cromai.log |sed 's/ /\n/g' 2>> cromai.log)
-echo $lista
-# Se a lista tem pids ativos então
-if [[ ! -z "$lista" ]] 2>> cromai.log; then
-	#procura nesta lista   
-	for item in $lista; do
-		# o número do pid que está gravado no arquivo 'pid' (cat pid).
-		# Se encontrar pid gravado no arquivo igual a um pid python ativo (`cat pid` == "$item" ) então
-		if [[ `cat pid` == "$item" ]] 2>> cromai.log; then
-			# variável executa recebe o valor "false"
-			executa=false
-			# imprime a frase "1: It is alive".
-			echo "1: It is alive"
-			# Finaliza esse loop.
-			break
-		fi
+	while true; do
+	# Procura por pid python ativo e verifica se um deles corresponde ao pid gravado no arquivo "pid". 
+	x=$(pidof -x python |sed 's/ /\n/g'| grep -e "`cat pid`$")
+	# Se achar imprime "It's alive caso, contrário imprime "It's dead e executa script python
+	if [[ ! -z "$x" ]]; then
+		echo "1: It is alive"
+	else
+		echo -e "1: It is dead"; python cromaipy.py & 2>>cromai.log | tee
+		sleep 2
+	fi
 	done
-fi
+# Cria arquivo "pid" caso ele não exista.
+[[ ! -e "pid" ]] 2>> cromai.log && touch pid
 
-#
-#Se a variável executa é true então, não existe programa python ativo (lista vazia) ou 
-#nenhum dos programas em python ativo é o cromaipy.py, então uma msg será impressa e o
-#programa cromaipy.py será executado.
-#
-
-if [[ $executa = true ]] 2>> cromai.log; then
-	echo "1: It is dead"
-	python cromaipy.py & 2>>cromai.log | tee
-sleep 2
-fi
-done
-
-# Se arquivo pid não existe grava erro no arquivo de erro "cromai.log".
-if [ ! -e "pid" ] 2>> cromai.log ; then
-	echo -e "Arquivo não encontrado, o arquivo 'pid' precisa estar na pasta corrente." >>  cromai.log 
-	echo -e "Arquivo não encontrado, log salvo em cromai.log."
-else
-	laco_infinito 2>> cromai.log
-fi
+# Chama função
+laco_infinito 2>> cromai.log
